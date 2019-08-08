dad0c34d420d0fb405a33ab631a88d0baac8950f
colemanirby <ckirbyee92@gmail.com>
2019-08-07 22:18:18 -0400

    Coleman - back to the beginning

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,16 +1,16 @@
 import {FrontendApiService} from '../frontend-api.service';
 import {FrontendModel} from '../frontend.model';
 import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
-import {Subscription} from "rxjs";
-import {MatTableDataSource} from "@angular/material";
-import {DirectoryModel} from "../directory.model";
+import {Subscription} from 'rxjs';
+import {MatTableDataSource} from '@angular/material';
+import {DirectoryModel} from '../directory.model';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   responseSubscription: Subscription;
   dataSource = new MatTableDataSource<DirectoryModel>();
@@ -20,14 +20,10 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnInit(): void {
-    this.callBackend();
+    this.frontendApi.getHelloWorld().subscribe((data: FrontendModel) => {
+      this.dataSource.data = data.directories;
+    });
     console.log('Response: ' + JSON.stringify(this.dataSource.data));
-  }
-  async callBackend() {
-    await this.frontendApi.getHelloWorld().toPromise().then(data => this.dataSource.data = data.directories);
-  }
-  ngAfterViewInit() {
-    console.log('Response after view: ' + this.dataSource.data);
   }
   ngOnDestroy() {
     this.responseSubscription.unsubscribe();
