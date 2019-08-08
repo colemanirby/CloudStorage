ec41854aa0389cfd6b92e6dccfee47f758ef8d05
colemanirby <ckirbyee92@gmail.com>
2019-08-07 21:15:33 -0400

    Coleman - pushed model change for frontend

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,6 +1,7 @@
 import {FrontendApiService} from '../frontend-api.service';
 import {FrontendModel} from '../frontend.model';
 import {Component, OnInit} from '@angular/core';
+import {DirectoryModel} from "../directory.model";
 
 @Component({
   selector: 'home',
@@ -9,7 +10,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  response: FrontendModel;
+  response: DirectoryModel[];
   displayedColumns: string[] = ['name'];
 
   constructor(private frontendApi: FrontendApiService) {
@@ -18,7 +19,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.frontendApi.getHelloWorld()
       .subscribe((data: FrontendModel) => {
-        this.response = data;
+        this.response = data.directories;
       }, console.error);
   }
 }
