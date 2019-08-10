45b59f066ada4423e9bbcc880d4db9bf87ca8702
Coleman Irby <ckirbyee92@gmail.com>
2019-08-10 00:16:31 -0400

    backend didn't work on raspbian. Issue fixed

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,7 +3,7 @@ import json
 
 from flask import Flask
 from flask_cors import CORS
-from backend.src.backend_service import FileService
+from src.backend_service import FileService
 
 
 app = Flask(__name__)
@@ -15,10 +15,11 @@ def return_directories():
     """For each directory in the directory tree rooted at top (including top
         itself, but excluding '.' and '..'), yields a 3-tuple
         dirpath, dirnames, filenames"""
-    os_walk = os.walk('../../backend')
+    os_walk = os.walk('./../backend')
 
     tuplet = next(os_walk)
     json_response = file_service.populate_directories_object({}, tuplet, os_walk)
-
+    
+    print('response:')
     print(json.dumps(json_response))
     return json.dumps(json_response)
