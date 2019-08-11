afc4cca7beb38e698828c04f7a21c2b0457af3be
colemanirby <ckirbyee92@gmail.com>
2019-08-11 03:43:31 -0400

    Coleman - Cleaned up directory names

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -25,8 +25,8 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.frontendApi.getHelloWorld().subscribe(data => {
       this.initialData = data;
       this.currentData = data;
-      this.setName(data.name);
       this.previousData.push(data);
+      this.setName(data.name);
       this.dataLoaded = true;
       console.log('Response: ' + JSON.stringify(this.initialData));
     });
@@ -51,17 +51,37 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.currentData = this.initialData;
     this.previousData = [];
     this.previousData.push(this.initialData);
-    this.setName(this.currentData.name)
+    this.setName(this.currentData.name);
   }
 
   setName(name: string): void {
-      console.log('name: ');
-      console.log(name);
+    console.log('name: ');
+    console.log(name);
+    if (this.previousData.length > 1) {
+      const previousName = this.previousData[this.previousData.length - 1].name;
+      const replaced = name.replace(this.previousData[this.previousData.length - 1].name + '/', '');
+      console.log('replaced:');
+      console.log(replaced);
+      this.name = replaced;
+    } else {
       const replaced = name.replace(/\D\W*/, '');
       console.log('replaced:');
       console.log(replaced);
       this.name = replaced;
+    }
   }
+
+  getDirectoryName(directoryName: string): string {
+    const replaced = directoryName.replace(this.currentData.name + '/', '');
+    console.log('replaced:');
+    console.log(replaced);
+    return replaced;
+  }
+
+  getPath(): string {
+    return this.currentData.name.replace(/\D\W*/, '');
+  }
+
 
   thereIsPreviousData(): boolean {
     return this.previousData.length > 1;
