07d4f4c9ac13636c7b3de42e1434b0d90db8116c
colemanirby <ckirbyee92@gmail.com>
2019-08-20 00:52:32 -0400

    Coleman - cleaned up python code

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,5 @@
 import os, stat
 import json
-import logging
 
 from flask import Flask
 from flask import request, send_from_directory, make_response
@@ -21,7 +20,7 @@ def return_directories():
     """For each directory in the directory tree rooted at top (including top
         itself, but excluding '.' and '..'), yields a 3-tuple
         dirpath, dirnames, filenames"""
-    
+
     os_walk = os.walk(basePath + currentExternalHD)
 
     tuplet = next(os_walk)
@@ -36,38 +35,30 @@ def return_directories():
 def download_file():
     file = request.files['file']
     path = request.form['path']
-    fileName = file.filename
-    
-    #might need to look into path that's passed in from the ui
-    save_file_path = path + '/' + fileName
+    file_name = file.filename
+
+    save_file_path = path + '/' + file_name
     print('saving file to')
     print(path)
-    
+
     print('file.filename')
     file.save(save_file_path)
 
     return 'Submitted'
 
+
 @app.route('/download/<string:path>/<string:number>/<string:filename>', methods=['GET'])
 def upload_file(path, number, filename):
-    
     print('received Request')
-    
-    #when I pass the path through the ui, I can't use / in the url since it will then be
-    #Interpereted as a continuation of the path so I use & then replace it with a / on arrival
-    #might not be good practice though.
-    finalPath = path.replace('&', '/').replace(' ', '\ ')
-    
+
+    final_path = path.replace('&', '/').replace(' ', '\ ')
+
     print('finalPath ----: ')
-    print(finalPath)
+    print(final_path)
     print('filename')
     print(filename)
-    
-    #need to get desired drive path here
-    #rename to current_drive
-        
-        
-    file = make_response(send_from_directory(directory=finalPath, filename=filename, as_attachment=True))
+
+    file = make_response(send_from_directory(directory=final_path, filename=filename, as_attachment=True))
     file.headers['File-Name'] = filename
-    
+
     return file
